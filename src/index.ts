631b9917b105c0393edb3eced8799a9bf0786fc6
Ruben Gees <rubengees7@googlemail.com>
2021-05-11 16:24:18 +0200

    Add logging, cache lookup and fix download links

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { addPath, getInput, setFailed } from "@actions/core"
-import { cacheFile, downloadTool } from "@actions/tool-cache"
-import * as fs from "fs"
+import { addPath, getInput, setFailed, info } from "@actions/core"
+import { cacheFile, downloadTool, find } from "@actions/tool-cache"
+import { promises as fs } from "fs"
 
-const DOWNLOAD_URL = "https://storage.yandexcloud.net/multiwerf/targets/releases/"
+const DOWNLOAD_URL = "https://storage.yandexcloud.net/multiwerf/targets/releases"
 
 async function run() {
   const version = getInput("multiwerf-version") || "1.4.7"
-  const multiwerfPath = await download(version)
+  const multiwerfPath = await downloadOrGetFromCache(version)
 
   switch (process.platform) {
     case "linux":
     case "darwin":
-      fs.chmodSync(multiwerfPath, 0o755)
+      await fs.chmod(multiwerfPath, 0o755)
       break
   }
 
@@ -25,13 +25,29 @@ async function run() {
   addPath(cachedPath)
 }
 
-async function download(version: string): Promise<string> {
+async function downloadOrGetFromCache(version: string): Promise<string> {
+  const existingPath = find("multiwerf", version)
+
+  if (existingPath) {
+    info(`multiwerf ${version} found in cache.`)
+
+    return existingPath
+  }
+
+  const link = getDownloadLink(version)
+
+  info(`Downloading multiwerf ${version} from ${link}...`)
+
+  return await downloadTool(link)
+}
+
+function getDownloadLink(version: string): string {
   if (process.platform === "win32") {
-    return await downloadTool(`${DOWNLOAD_URL}/${version}/multiwerf-windows-amd64-v${version}.exe`)
+    return `${DOWNLOAD_URL}/v${version}/multiwerf-windows-amd64-v${version}.exe`
   } else if (process.platform == "darwin") {
-    return await downloadTool(`${DOWNLOAD_URL}/${version}/multiwerf-darwin-amd64-v${version}`)
+    return `${DOWNLOAD_URL}/v${version}/multiwerf-darwin-amd64-v${version}`
   } else if (process.platform == "linux") {
-    return await downloadTool(`${DOWNLOAD_URL}/${version}/multiwerf-linux-amd64-v${version}`)
+    return `${DOWNLOAD_URL}/v${version}/multiwerf-linux-amd64-v${version}`
   } else {
     throw new Error(`Unsupported platform: ${process.platform}`)
   }
